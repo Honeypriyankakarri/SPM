9cad0f7cc2fe760d93b7f342dedd861c28588948
Honeypriyankakarri <khoneypriyanka@gmail.com>
2023-04-03 12:07:39 +0530

    1st sommit

---
+++ tokenreset.py
@@ -0,0 +1,5 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+def token(rollno,seconds):
+     s=Serializer('h&YwehiGfty&',seconds)
+     return s.dumps({'user':rollno}).decode('utf-8')
+    
